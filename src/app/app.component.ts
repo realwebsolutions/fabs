9bc6b065703a44a3f44e1e04b322e06b8c720592
aashinsiby <aashin2000@gmail.com>
2024-07-24 12:33:35 +0530

    1.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutusComponent } from "./aboutus/aboutus.component";
+import { ServicesComponent } from "./services/services.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, AboutusComponent]
+    imports: [RouterOutlet, AboutusComponent, ServicesComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'fab';
